ade685b346ec8938f43add27de08f3a5ef41c305
github username <github email address>
2021-06-30 15:09:06 -0400

    commit tree 88d312fc6e875cb2f30d17ea26d50ac978e2d19c

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+const EyesOnMe = (props) => {
+  const handleBlur = () => console.log("Hey! Eyes on me!");
+  const handlefocus = () => console.log("Good!");
+
+  return (
+    <button onFocus={handlefocus} onBlur={handleBlur}>
+      Eyes on me
+    </button>
+  );
+};
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,9 @@
 // Code Keypad Component Here
+const Keypad = (props) => {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+  return <input type="password" onChange={handleChange}></input>;
+};
+
+export default Keypad;
